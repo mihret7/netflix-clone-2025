ccf4dc2ac6cf1886df891571e44ac81b840ef3ca
Mihret7 <mihretbizuayehu777@gmail.com>
2025-05-14 15:33:39 +0300

    update

--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -10,30 +10,41 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div className='header_outer_container'>
-      <div className='header_container'>
-        <div className='header_left'>
-            <ul>
-                <li><img src={NetflixLogo} alt="Netflix Logo" width="100" /></li>
-                <li>Home</li>
-                <li>TVShows</li>
-                <li>Movies</li>
-                <li>Latest</li>
-                <li>MyList</li>
-                <li>Browse by Languages</li>
-            </ul>
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
+            </li>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Games</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
         </div>
-        <div className='header_right'>
-            <ul>
-                <li><SearchIcon /></li>
-                <li><NotificationsNoneIcon /></li>
-                <li><AccountBoxIcon /></li>
-                <li><ArrowDropDownIcon /></li>
-            </ul>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+
+            <li>Kids</li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Header
